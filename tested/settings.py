536789c70a22cbfd8688556bb2691b29620e0259
Roderick Bompart <rbompart9@gmail.com>
2023-04-17 05:29:42 -0400

    chore: :wrench: Remove warnings

--- tested/settings.py
+++ tested/settings.py
@@ -110,8 +110,6 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
-
 USE_TZ = True
 
 
@@ -119,3 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
